b49643a7d34640978f0dc1afe7b7800d9dfaeb92
JasmineSun <2043281367@qq.com>
2019-08-19 11:03:33 +0800

    leetCode modified

--- LeetCode/src/study/Day0816/Solution.java
+++ LeetCode/src/study/Day0816/Solution.java
@@ -3,35 +3,55 @@ package study.Day0816;
 import study.BackTrackingMethod.ListSort;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 public class Solution {
-    public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        List<Integer> aIntegers = new ArrayList<>();
 
-        solve(aIntegers, 0, nums, list);
-        return list;
+    private List<List<Integer>> res = new ArrayList<>();
+    private boolean[] used;
+
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        int len = nums.length;
+        if (len == 0) {
+            return res;
+        }
+        // 排序，之后才有可能发现重复分支
+        Arrays.sort(nums);
+
+        used = new boolean[len];
+        findPermuteUnique(nums, 0, new Stack<>());
+        return res;
+
+
     }
 
-    private static void solve(List<Integer> aIntegers, int n, int[] nums, List<List<Integer>> list) {
-        if (n == nums.length) {
-            list.add(new ArrayList<>(aIntegers));
+    private void findPermuteUnique(int[] nums, int depth, Stack<Integer> stack) {
+        if (depth == nums.length) {
+            res.add(new ArrayList<>(stack));
             return;
         }
-
         for (int i = 0; i < nums.length; i++) {
-            if (aIntegers.contains(nums[i])) {
-                continue;
+            if (!used[i]) {
+                // 因为排序以后重复的数一定不会出现在开始，故 i > 0
+                // 和之前的数相等，并且之前的数还未使用过，只有出现这种情况，才会出现相同分支，这种情况跳过即可
+                if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+                    continue;
+                }
+                used[i] = true;
+                stack.add(nums[i]);
+                findPermuteUnique(nums, depth + 1, stack);
+                stack.pop();
+                used[i] = false;
             }
-            aIntegers.add(nums[i]);
-            solve(aIntegers, n + 1, nums, list);
-            aIntegers.remove(aIntegers.size() - 1);
         }
     }
 
+
     public static void main(String[] args) {
-        Solution s  = new Solution();
+        Solution s = new Solution();
         int[] nums = new int[]{1, 1, 2};
         List<List<Integer>> result = s.permuteUnique(nums);
         for (int i = 0; i < result.size(); i++) {
